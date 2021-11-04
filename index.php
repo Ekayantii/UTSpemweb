bb7c214659da1cf0deaaa6f8f9a7ee0590a6583f
Ekayantii <90376430+Ekayantii@users.noreply.github.com>
2021-11-04 23:01:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once("berita1.html");?>
+<?php include_once("berita2.html");?>
+<?php include_once("berita3.html");?>
+<?php include_once("dosen.html");?>
+<?php include_once("index.html");?>
+<?php include_once("sejarah.html");?>
+<?php include_once("struktur.html");?>
+<?php include_once("visimisi.html");?>
